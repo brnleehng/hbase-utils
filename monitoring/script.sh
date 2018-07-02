d763f094c5ffdac844fd162cdf2d90c378653ec4
Brian <l.brian.hoang@gmail.com>
2018-07-02 15:07:54 -0700

    Updated monitoring script

--- monitoring/script.sh
+++ monitoring/script.sh
@@ -3,14 +3,6 @@
 wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_GA_v1.2.0-25/omsagent-1.2.0-25.universal.x64.sh -O /tmp/omsagent.x64.sh
 sudo sh /tmp/omsagent.x64.sh --upgrade -w $1 -s $2
 
-# if [[ $HOSTNAME == wn* ]];
-# then
-#   sudo wget https://raw.githubusercontent.com/Azure/hbase-utils/master/monitoring/hbase.workernode.conf -O /etc/opt/microsoft/omsagent/conf/omsagent.d/hbase.workernode.conf
-# elif [[ $HOSTNAME == zk* ]];
-# then
-#   sudo wget https://raw.githubusercontent.com/Azure/hbase-utils/master/monitoring/hbase.zookeeper.conf -O /etc/opt/microsoft/omsagent/conf/omsagent.d/hbase.zookeeper.conf
-# fi
-
 sudo wget https://raw.githubusercontent.com/brnleehng/hbase-utils/feature/batch-oms/monitoring/batch.workernode.conf -O /etc/opt/microsoft/omsagent/conf/omsagent.d/batch.workernode.conf
 
 sudo wget https://raw.githubusercontent.com/brnleehng/hbase-utils/feature/batch-oms/monitoring/filter_hdinsight.rb -O /opt/microsoft/omsagent/plugin/filter_hdinsight.rb
